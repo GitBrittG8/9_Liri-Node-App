f8ddd5f6091758d8a24b0aeda1f2690f96d35a17
Rhubarb <edlun066@d.umn.edu>
2019-05-20 21:21:19 -0500

    OMBD Release Year Insert(18)

---
+++ liri.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+// Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
+var axios = require("axios");
